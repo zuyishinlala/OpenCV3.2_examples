706597becf4b14f4c2d24f1a81e22853df180d9f
Chi-Sheng <s410985034@gm.ntpu.edu.tw>
2024-02-05 22:30:45 +0800

    third commit

--- ReadFile.c
+++ ReadFile.c
@@ -16,13 +16,13 @@
 #define NUM_CLASSES 80
 #define NUM_MASKS 32
 
-#define WIDTH0 TRAINED_SIZE_WIDTH / 8  // 0
-#define WIDTH1 TRAINED_SIZE_WIDTH / 16 // 1
-#define WIDTH2 TRAINED_SIZE_WIDTH / 32 // 2
+#define WIDTH0 TRAINED_SIZE_WIDTH/8
+#define WIDTH1 TRAINED_SIZE_WIDTH/16
+#define WIDTH2 TRAINED_SIZE_WIDTH/32
 
-#define HEIGHT0 TRAINED_SIZE_HEIGHT / 8
-#define HEIGHT1 TRAINED_SIZE_HEIGHT / 16
-#define HEIGHT2 TRAINED_SIZE_HEIGHT / 32
+#define HEIGHT0 TRAINED_SIZE_HEIGHT/8
+#define HEIGHT1 TRAINED_SIZE_HEIGHT/16
+#define HEIGHT2 TRAINED_SIZE_HEIGHT/32
 
 #define ROWSIZE HEIGHT0*WIDTH0 + HEIGHT1*WIDTH1 + HEIGHT2*WIDTH2
 
@@ -36,22 +36,7 @@ int main(int argc,char** argv){
 
     float reg_pred[ROWSIZE][4]; // position
 
-    float seg_pred[ROWSIZE][1+NUM_MASKS]; // proto net index, mask 
-
-    float masks[NUM_MASKS][MASK_SIZE_HEIGHT*MASK_SIZE_WIDTH];
-    /*
-    ============================================
-        To-do: Read files and store data into arrays
-    ============================================    
-    */
-    sigmoid(&cls_pred, ROWSIZE, NUM_CLASSES);
-}
-
-static void sigmoid(float** arr, int rowsize, int colsize)
-{
-    for(int r = 0 ; r < rowsize; r++){
-        for(int c = 0 ; c < colsize; c++){
-            arr[r][c] = 1.0f / (1.0f + fast_exp(-arr[r][c]));
-        }
-    }
+    float seg_pred0[1+NUM_MASKS][HEIGHT0][WIDTH0];
+    float seg_pred1[1+NUM_MASKS][HEIGHT1][WIDTH1];
+    float seg_pred2[1+NUM_MASKS][HEIGHT2][WIDTH2];
 }
